751fc9a0993206426109d27d1b823b83be00850e
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2013-03-24 20:51:30 -0300

    added video modal

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -17,26 +17,46 @@ universalSearchApp.controller('universalSearchCtrl', function($scope, $resource,
     $scope.searchYoutube = function(){
       var q = $scope.searchTerm;
 
-      // Check if the query was already performed
-      if (YoutubeCache.get(q)) {
+      //YoutubeCache.get(q)
+      //YoutubeCache.put(q,$scope.youtubeResults); 
+
+      if( YoutubeCache.get(q) ){
 
         $scope.youtubeResults = YoutubeCache.get(q);
-        console.log('Query pulled from cache.');
+        console.log('Resultados retirados do cache. Nenhum requisição foi feita');
 
       } else {
+
         $scope.youtube = $resource('https://gdata.youtube.com/feeds/api/videos',
           {q: q, callback: 'JSON_CALLBACK', alt: 'json'},
           {get:{method:'JSONP'}});
 
         $scope.youtubeResults = $scope.youtube.get();
 
-        // Store this query in cache
         YoutubeCache.put(q,$scope.youtubeResults); 
+        console.log('Resultados retornados da API do youtube.');
 
-        console.log('Writting results in cache..');       
-      };
-            
+      }
     }
+
+    $scope.playVideo = function(item) {
+        var vid = $scope.youtube_parser(item.link[0].href);
+        $scope.item_title = item.title.$t;
+        $scope.item_thumb = item.media$group.media$thumbnail[3].url;
+        $scope.item_iframe = "http://www.youtube.com/embed/" + vid;
+        
+        $('#VideoModal').modal('show');
+    }
+
+    $scope.youtube_parser = function(url){
+        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+        var match = url.match(regExp);
+        if (match&&match[7].length==11){
+            return match[7];
+        }else{
+            return false;
+        }
+    }    
 });
 
 
